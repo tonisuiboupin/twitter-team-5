d8c02e6f9ab78cb5a3ad42bcbe98d58942127d91
tonis.uiboupin <tonis.uiboupin@iglu.ee>
2018-06-28 12:04:34 +0300

    springboot gradle rakendus

---
+++ src/main/java/com/example/iglutwitter/IgluTwitterApplication.java
@@ -0,0 +1,12 @@
+package com.example.iglutwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgluTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgluTwitterApplication.class, args);
+	}
+}
