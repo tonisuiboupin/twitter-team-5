2444ace2e449aa9b665bb9123116488ff8402ceb
toomluu <toomas.luus@iglu.ee>
2018-06-28 15:57:23 +0300

    teenused

--- backend/src/main/java/com/example/iglutwitter/controller/AuthenticationController.java
+++ backend/src/main/java/com/example/iglutwitter/controller/AuthenticationController.java
@@ -1,15 +1,29 @@
 package com.example.iglutwitter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.iglutwitter.service.AuthenticationService;
+
 @RestController("/api/auth")
 public class AuthenticationController{
+    
+    @Autowired
+    private AuthenticationService authenticationService;
 
     @GetMapping(params = {"name", "password"})
     public ResponseEntity<Boolean> authenticate( @RequestParam("name") String name, @RequestParam("password") String password ){
         return ResponseEntity.ok( name != null && name.equals( password ) );
+    }
+    
+    //@PostMapping(params = {"name", "password"})
+    @RequestMapping("/api/user/post")
+    public String createAccount( @RequestParam("name") String accountName, @RequestParam("password") char[] password ){
+        return authenticationService.createAccount(accountName, password);
     }
 }
