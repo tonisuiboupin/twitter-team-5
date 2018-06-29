5b47b7227f5ad5aa33d700f47797bc5be72ba490
aivo.kangsepp <aivo.kangsepp@iglu.ee>
2018-06-29 11:38:04 +0300

    login

--- backend/src/main/java/com/example/iglutwitter/authentication/JWTLoginFilter.java
+++ backend/src/main/java/com/example/iglutwitter/authentication/JWTLoginFilter.java
@@ -1,8 +1,12 @@
 package com.example.iglutwitter.authentication;
 
-import com.example.iglutwitter.model.User;
-import com.example.iglutwitter.repository.UserRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,18 +15,15 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Collections;
+import com.example.iglutwitter.model.User;
+import com.example.iglutwitter.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter{
 
     private final UserRepository userRepository;
-    
-    public JWTLoginFilter( String url, AuthenticationManager authManager, UserRepository userRepository){
+
+    public JWTLoginFilter( String url, AuthenticationManager authManager, UserRepository userRepository ){
         super( new AntPathRequestMatcher( url ) );
         setAuthenticationManager( authManager );
         this.userRepository = userRepository;
@@ -32,13 +33,16 @@ public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter{
     public Authentication attemptAuthentication(
                                                  HttpServletRequest req,
                                                  HttpServletResponse res ) throws AuthenticationException, IOException, ServletException{
-        User user = new ObjectMapper()
-                .readValue( req.getInputStream(), User.class );
-        return getAuthenticationManager().authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        user.getUserName(),
-                        user.getPassword(),
-                        Collections.emptyList() ) );
+        if( req.getMethod().equals( "POST" ) ){
+            User user = new ObjectMapper()
+                    .readValue( req.getInputStream(), User.class );
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            user.getUserName(),
+                            user.getPassword(),
+                            Collections.emptyList() ) );
+        }
+        return null;
     }
 
     @Override
