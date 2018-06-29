bf949e8f5b2ea34bc3dc536bf1084880bb6c7b34
aivo.kangsepp <aivo.kangsepp@iglu.ee>
2018-06-29 13:12:48 +0300

    twiiit

--- backend/src/main/java/com/example/iglutwitter/controller/IgluTwitterController.java
+++ backend/src/main/java/com/example/iglutwitter/controller/IgluTwitterController.java
@@ -1,24 +1,36 @@
 package com.example.iglutwitter.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigInteger;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.iglutwitter.authentication.TokenAuthenticationService;
 import com.example.iglutwitter.dto.TweetDto;
 import com.example.iglutwitter.model.User;
 import com.example.iglutwitter.service.IgluTwitterService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
@@ -26,15 +38,32 @@ public class IgluTwitterController{
 
     private final IgluTwitterService twitterService;
 
+    private static final ObjectMapper objectMapper = new ObjectMapper().registerModule( new ParameterNamesModule() )
+            .registerModule( new Jdk8Module() ).registerModule( new JavaTimeModule() );
+
     @RequestMapping("/")
     public String index(){
         return "Greetings from Iglu Twitter!";
     }
 
+    @Getter
+    @ToString
+    @RequiredArgsConstructor
+    public static class TweetRequest{
+        private final String txt;
+    }
+
     // TODO autentimise kontroll, userId peaks tulema autentimise tokenist
     @RequestMapping("/api/tweet/post")
-    public void twiiiiiit( @RequestParam String txt, @RequestHeader(TokenAuthenticationService.HEADER_STRING) String jwt ){
-        twitterService.add( new BigInteger( TokenAuthenticationService.getUserIdFromJWT( jwt ) ), txt );
+    public void twiiiiiit( @RequestHeader(TokenAuthenticationService.HEADER_STRING) String jwt,
+                           HttpServletRequest request ) throws IOException{
+        try (InputStream is = request.getInputStream()){
+            String content = IOUtils.toString( is, "UTF-8" );
+            TweetRequest tweet = objectMapper.readValue( content, TweetRequest.class );
+            String userId = TokenAuthenticationService.getUserIdFromJWT( jwt );
+            log.info( "Add twiit: {}, userId: {}", tweet, userId );
+            twitterService.add( new BigInteger( userId, 10 ), tweet.getTxt() );
+        }
     }
 
     @GetMapping("/api/user/{userId}/tweets")
