352287078c1939235afdcc1e830b2f48206b37b2
toomluu <toomas.luus@iglu.ee>
2018-06-29 09:06:51 +0300

    jwt

--- backend/src/main/java/com/example/iglutwitter/authentication/WebSecurityConfig.java
+++ backend/src/main/java/com/example/iglutwitter/authentication/WebSecurityConfig.java
@@ -21,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         http.csrf().disable().authorizeRequests()
                 .antMatchers( "/" ).permitAll()
                 .antMatchers( HttpMethod.POST, "/login" ).permitAll()
+                .antMatchers( "/api/user/**" ).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore( filter,
